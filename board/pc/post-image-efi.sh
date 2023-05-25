91cf4f49d3fef99b39436c0e7fc29c5769520045
Timo van Sark <timovansark@live.nl>
2023-05-25 18:55:33 +0200

    bzImage.efi console=ttyS0

--- board/pc/post-image-efi.sh
+++ board/pc/post-image-efi.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 set -e
+set -x
 
-UUID=$(dumpe2fs "$BINARIES_DIR/rootfs.ext2" 2>/dev/null | sed -n 's/^Filesystem UUID: *\(.*\)/\1/p')
+echo $(dumpe2fs "$BINARIES_DIR/rootfs.cpio")
+
+UUID=$(dumpe2fs "$BINARIES_DIR/rootfs.cpio" 2>/dev/null | sed -n 's/^Filesystem UUID: *\(.*\)/\1/p')
 sed -i "s/UUID_TMP/$UUID/g" "$BINARIES_DIR/efi-part/EFI/BOOT/grub.cfg"
 sed "s/UUID_TMP/$UUID/g" board/pc/genimage-efi.cfg > "$BINARIES_DIR/genimage-efi.cfg"
 support/scripts/genimage.sh -c "$BINARIES_DIR/genimage-efi.cfg"
