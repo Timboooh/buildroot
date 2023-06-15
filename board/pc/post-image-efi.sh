00f0d4aa6985fdd087ab08fd22f4aa257c8b1864
Timo van Sark <timo.van.sark@prodrive-technologies.com>
2023-06-15 16:33:32 +0200

    where was I

--- board/pc/post-image-efi.sh
+++ board/pc/post-image-efi.sh
@@ -6,7 +6,7 @@ set -x
 # UUID=$(dumpe2fs "$BINARIES_DIR/rootfs.ext2" 2>/dev/null | sed -n 's/^Filesystem UUID: *\(.*\)/\1/p')
 # sed -i "s/UUID_TMP/$UUID/g" "$BINARIES_DIR/efi-part/EFI/BOOT/grub.cfg"
 BZIMAGE_SIZE=$(stat --format='%s' "$BINARIES_DIR/bzImage")
-BZIMAGE_SIZE=$(($BZIMAGE_SIZE+65536))
+BZIMAGE_SIZE=$(($BZIMAGE_SIZE+131072)) # +128 KiB
 sed "s/BZIMAGE_SIZE/$BZIMAGE_SIZE/g" board/pc/genimage-efi.cfg > "$BINARIES_DIR/genimage-efi.cfg"
 
 # cp board/pc/genimage-efi.cfg "$BINARIES_DIR/genimage-efi.cfg"
